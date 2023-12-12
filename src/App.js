2f42f8c92726a0d9a4aca5be3ef662d95d16fc50
Hanna <akhhanna20@gmail.com>
2023-12-11 23:49:59 -0500

    Added checkbox, added Drag & Drop, changed some styles

--- src/App.js
+++ src/App.js
@@ -2,16 +2,58 @@ import React, { useEffect, useState } from "react";
 import TodoList from "./TodoList";
 import AddTodoForm from "./AddTodoForm";
 import "./app.css";
+//require("dotenv").config();
+//import "dotenv/config";
+//require("dotenv").config();
 
 function App() {
+  console.log("env", process.env.REACT_APP_TABLE_NAME);
   const today = new Date();
 
-  //This state change the list of todos method and retrieves objects from localStorage;
-  //When we stored the data, we first converted it to a JSON string.
-  //In order to make use of it, we need to convert JSON string back to a JSON object.
   const [todoList, setTodoList] = useState([]);
 
   const [isLoading, setIsLoading] = useState(true);
+
+  const [dragId, setDragId] = useState();
+
+  // const fetchData = async () => {
+  //   const options = {
+  //     method: "GET",
+  //     headers: {
+  //       Authorization: `Bearer ${process.env.REACT_APP_AIRTABLE_API_TOKEN}`,
+  //     },
+  //   };
+  //   const url = `https://api.airtable.com/v0/${process.env.REACT_APP_AIRTABLE_BASE_ID}/${process.env.REACT_APP_TABLE_NAME}/`;
+
+  //   try {
+  //     const response = await fetch(url, options);
+
+  //     if (!response.ok) {
+  //       const message = `Error: ${response.status}`;
+  //       throw new Error(message);
+  //     }
+
+  //     const data = await response.json();
+  //     return data;
+  //     console.log(data);
+  //   } catch (error) {
+  //     console.log(error.message);
+  //   }
+  // };
+
+  // useEffect(() => {
+  //   fetchData().then((data) => {
+  //     const todos = data.records.map((todo) => {
+  //       const newTodo = {
+  //         title: todo.fields.title,
+  //         id: todo.id,
+  //       };
+  //       return newTodo;
+  //     });
+  //     setTodoList(todos);
+  //     setIsLoading(false);
+  //   });
+  // }, []);
 
   useEffect(() => {
     new Promise((resolve, reject) => {
@@ -20,6 +62,9 @@ function App() {
       setTimeout(
         () =>
           resolve({
+            //This state change the list of todos method and retrieves objects from localStorage;
+            //When we stored the data, we first converted it to a JSON string.
+            //In order to make use of it, we need to convert JSON string back to a JSON object.
             data: {
               todoList: JSON.parse(localStorage.getItem("savedTodoList")) || [],
             },
@@ -34,35 +79,94 @@ function App() {
 
   //setItem is used to store objects in localStorage.
   //To store data in localStorage, you must first stringify it.
+
+  const reorderItems = (newTodoList) => {
+    const newItems = newTodoList.map((todo, index) => {
+      todo.order = index;
+      return todo;
+    });
+    setTodoList(newItems);
+  };
+
+  //Callback handler, to ad New Todo to List
+  const addTodo = (newTodo) => {
+    setTodoList((todoList) => [
+      ...todoList,
+      { ...newTodo, done: false, order: todoList.length },
+    ]);
+  };
+  // console.log(todoList);
+
+  const removeTodo = (id) => {
+    const newTodoList = todoList.filter((todo) => todo.id !== id);
+    //setTodoList(newTodoList);
+    reorderItems(newTodoList);
+  };
+
+  const handleCheckboxChange = (id) => {
+    const newTodoList = todoList.map((todo) =>
+      todo.id === id ? { ...todo, done: !todo.done } : todo
+    );
+    setTodoList(newTodoList);
+  };
+
   useEffect(() => {
     if (!isLoading) {
       localStorage.setItem("savedTodoList", JSON.stringify(todoList));
     }
   });
 
-  //Callback handler, to ad New Todo to List
-  const addTodo = (newTodo) => {
-    setTodoList((todoList) => [...todoList, newTodo]);
-  };
+  const handleDrag = (event) => {
+    setDragId(event.currentTarget.id);
 
-  const removeTodo = (id) => {
-    const newTodoList = todoList.filter((el) => el.id !== id);
-    setTodoList(newTodoList);
+    console.log("dragID", event.currentTarget.id);
+    console.log("dragIDTYPE", typeof event.currentTarget.id);
+  };
+  console.log("todoList", todoList);
+
+  const handleDrop = (event) => {
+    const dragBoxIndex = todoList.findIndex(
+      (todo) => todo.id.toString() === dragId
+    );
+    console.log("drag", dragBoxIndex);
+    const dropBoxIndex = todoList.findIndex(
+      (todo) => todo.id.toString() === event.currentTarget.id
+    );
+
+    console.log("dropInd", dropBoxIndex);
+    console.log("dropID", event.currentTarget.id);
+
+    const newTodoState = todoList.map((todo) => {
+      if (todo.id.toString() === dragId) {
+        todo.order = dropBoxIndex;
+      }
+      if (todo.id.toString() === event.currentTarget.id) {
+        todo.order = dragBoxIndex;
+      }
+      return todo;
+    });
+
+    setTodoList(newTodoState);
   };
 
   return (
     <div className="todo-wrapper">
+      <h1>Todo List</h1>
+      <h3>
+        Date: {today.getMonth() + 1}/{today.getDate()}/{today.getFullYear()}
+      </h3>
+      <AddTodoForm onAddTodo={addTodo} />
+
       {isLoading ? (
-        <p>Loading...</p>
+        <p>Loading your todos...</p>
       ) : (
-        <div>
-          <h1>Todo List</h1>
-          <h3>
-            Date: {today.getMonth() + 1}/{today.getDate()}/{today.getFullYear()}
-          </h3>
-          <AddTodoForm onAddTodo={addTodo} />
-          <TodoList todoList={todoList} onRemoveTodo={removeTodo} />
-        </div>
+        <TodoList
+          todoList={todoList}
+          onRemoveTodo={removeTodo}
+          handleCheckboxChange={handleCheckboxChange}
+          handleDrag={handleDrag}
+          handleDrop={handleDrop}
+        />
       )}
     </div>
   );
--- src/TodoListItem.js
+++ src/TodoListItem.js
@@ -1,16 +1,37 @@
 import React from "react";
 import { GoTrash } from "react-icons/go";
 
-function TodoListItem({ todo, onRemoveTodo, listItem }) {
+function TodoListItem({
+  todo,
+  onRemoveTodo,
+  listItem,
+  handleCheckboxChange,
+  handleDrag,
+  handleDrop,
+}) {
   return (
-    <li className="todo-list-item">
-      {todo}
-      {/* <button > */}
+    <li
+      className="todo-list-item"
+      draggable={true}
+      id={listItem.id}
+      onDragOver={(event) => event.preventDefault()}
+      onDragStart={handleDrag}
+      onDrop={handleDrop}
+    >
+      <input
+        type="checkbox"
+        id="listItem"
+        name="listIitem"
+        checked={listItem.done}
+        onChange={() => handleCheckboxChange(listItem.id)}
+      />
+
+      <div className={listItem.done ? "close" : ""}>{todo}</div>
+
       <GoTrash
         className="icon-delete"
         onClick={() => onRemoveTodo(listItem.id)}
       />
-      {/* </button> */}
     </li>
   );
 }
